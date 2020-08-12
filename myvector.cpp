779c92dedd6eabab4f8fa38e3ba97ba329f48a54
MasterTinka <62104732+MasterTinka@users.noreply.github.com>
2020-08-12 23:52:11 +0600

    New Version
    Updated

--- myvector.cpp
+++ myvector.cpp
@@ -2,13 +2,32 @@
 #include <iostream>
 
 template<class typeOf>
-MyVector<typeOf>::MyVector() //Конструктор
+MyVector<typeOf>::MyVector() //Конструктор по умолчанию
 {
 
 }
 
 template<class typeOf>
-MyVector<typeOf>::~MyVector()
+MyVector<typeOf>::MyVector(const MyVector &vector)              // Конструктор копирования
+{
+    this->memory = vector.memory;                               // Копируется память
+    this->vectorContains = vector.vectorContains;               // Копируется содержимое массива
+}
+
+template<class typeOf>
+MyVector<typeOf>::MyVector(typeOf fillElement, int repeat)      // Конструктор заполнения
+{
+    this->push_back(fillElement, repeat);                       // Вызывается функция, кладущая в конец элемент, n-ое колличество раз
+}
+
+template<class typeOf>
+MyVector<typeOf>::MyVector(typeOf pushElement)                  // Конструктор, кладущий одиночный элемент
+{
+    this->push_back(pushElement);                               // Вызывается функция, кладущая в конец элемент
+}
+
+template<class typeOf>
+MyVector<typeOf>::~MyVector()                                   // Деструктор по умолчанию
 {
     delete[] vectorContains;
 }
@@ -22,7 +41,7 @@ unsigned int MyVector<typeOf>::size() const noexcept
 template<class typeOf>
 unsigned int MyVector<typeOf>::capacity() const noexcept
 {
-    return sizeof (memory) + sizeof (typeOf) * memory.total; //Вернуть количество занятой памяти
+    return sizeof (typeOf) * memory.total; //Вернуть количество занятой памяти
 }
 
 template<class typeOf>
@@ -39,89 +58,152 @@ void MyVector<typeOf>::resize(int size)
     updateVector();      // Обновляется вектор
 }
 
-template <class typeOf>
-void MyVector<typeOf>::shrink_to_fit()
+template<class typeOf>
+void MyVector<typeOf>::upscale(int size)
 {
-    if(memory.free)
+     this->resize(memory.total + (size - memory.free));           // Увеличивает массив на size, но при этом учитывает свободную память
+}
+
+template <class typeOf>
+void MyVector<typeOf>::shrink_to_fit() const noexcept
+{
+    if(memory.free)                                               // Если память свободна
     {
-        this->resize(memory.total - memory.free);
+        this->resize(memory.total - memory.free);                 // Изменить размер вектора, чтобы памяти свободной не оставалось
     }
 }
 
 template<class typeOf>
 void MyVector<typeOf>::push_back(typeOf pushedBack)             // Положить элемент в конец вектора
 {
-    if(!memory.free)                                            // если память не свободна
+    if(memory.free)                                 // Если память свободна
     {
-        updateMemory(1);                                     // увеличить на 1, и сразу её занять
+        vectorContains[memory.used] = pushedBack;   // Положить элемент на memory.used место, т.е. на последнее доступное
+        memory.used++;
+        memory.lastIndex++;
+        updateMemory(0);                            // Обновление memory.free
     }
-
     else
     {
-        memory.used++;
-        updateMemory(0);
+        updateVector();                             // Обновление вектора
+        vectorContains[memory.used] = pushedBack;   // На последнее место ложится элемент
+        memory.lastIndex++;
+        updateMemory(1);                            // Увеличение memory.total и memory.used на 1;
     }
-
-    updateVector();                                             // Обновление размера вектора
-
-    vectorContains[memory.lastIndex] = pushedBack;       // На последнюю позицию ложатся данные
-
-    memory.lastIndex++;
 }
 
 template<class typeOf>
-void MyVector<typeOf>::pop_back()             // Положить элемент в конец вектора
+void MyVector<typeOf>::push_back(typeOf pushed_back, int repeat)    // Положить элемент в конец n-ое колличество раз
+{
+    this->upscale(repeat);            // Вектор увеличивается в размере, что-бы вместетить repeat колличество элементов
+    for(int i = 0; i < repeat; i++)
+        this->push_back(pushed_back); // Вызывается метод push_back, с определённым элементом
+}
+
+template<class typeOf>
+void MyVector<typeOf>::insert(typeOf element, int position)
+{
+    if(position == memory.used)                         // Если вставляется на последнее место
+    {
+        push_back(element);                             // Просто вызывается метод push_back
+    }
+    else if (position == memory.used - 1)
+    {
+        typeOf tempElement = vectorContains[memory.lastIndex];
+        this->pop_back();                               // Удалить последний элемент
+        this->push_back(element);                       // Положить туда вставляемый элемент
+        this->push_back(tempElement);                   // Положить убранный элемент обратно
+    }
+    else                                                // В любом другом случае
+    {
+        updateMemory(1);                                // Увеличить память на 1, и сразу её занять
+        for(int i = position - 1; i < memory.used; i++) // Начиная с position элемента массива, до его конца
+        {
+            typeOf tempElement1, tempElement2;          // Создаётся два временных элемета
+            if(i == position - 1)                       // Если это первый проход цикла
+            {
+                tempElement1 = vectorContains[i];       // Первый временный элемент копирует значения массива, на месте вставки
+                vectorContains[i] = element;            // И заменяет его вставляемым element`ом
+            }
+            else
+            {
+                tempElement2 = vectorContains[i];       // Второй временный элемент копирует значение массива на следующем месте
+                vectorContains[i] = tempElement1;       // В массив записывается элемент, стоявший на месте i-1
+                tempElement1 = tempElement2;            // Первый временный элемент перенимает значение второго
+            }
+        }
+        memory.lastIndex++;
+    }
+}
+
+template<class typeOf>
+void MyVector<typeOf>::insert(typeOf element, int position, int repeat)
+{
+    for(int i = 0; i < repeat; i++)                 // Пока i меньше числа повторов
+    {
+        this->insert(element, position);            // Вызывается insert с элементом и позицием
+        position++;
+    }
+}
+
+template<class typeOf>
+void MyVector<typeOf>::pop_back()                   // Убрать элемент из конца вектора
 {
     if(memory.total > 0)
     {
-        memory.total--;                             //Уменьшить память
-
-        if(!memory.free)
-            memory.used--;
-
-        updateMemory(0);                            // Обновление свободной памяти
-        updateVector();                             // Обновление размера вектора
-        memory.lastIndex--;
+        vectorContains[memory.lastIndex] = 0;       // Последний элемент равняется 0
+        memory.used -= 1;                           // Использованная память уменьшается на 1
+        memory.lastIndex -= 1;                      // Последний индекс уменьшается на 1
+        updateMemory(0);                            // Обновляется свободная память
+    }
+    else
+    {
+        throw std::out_of_range("MyVector::pop_back(), Vector is empty");
     }
 }
 
 template<class typeOf>
-void MyVector<typeOf>::clear()                      // Очищает вектор
+void MyVector<typeOf>::clear()     // Очищает вектор
 {
     this->resize(0);
 }
 
 
 template <class typeOf>
-typeOf MyVector<typeOf>::at(int index)
+typeOf& MyVector<typeOf>::at(int index)
 {
-    if(index > this->memory.used) //если индекс больше, чем максимальный
+    if(index > this->memory.total) //если индекс больше, чем максимальный
     {
-        return typeOf(); //ничего не возвращать
+        throw std::out_of_range("MyVector::at(int index), index is out of range");       //Бросаем эксепшн
     }
     else  // иначе
     {
         return vectorContains[index - 1]; //Вернуть элемент на i-1 месте
     }
-
 }
 
 template<class typeOf>
-typeOf MyVector<typeOf>::front()
+typeOf& MyVector<typeOf>::front()
 {
     if(memory.used)                         // Если есть занятая память
-        return vectorContains[0];    // Вернуть первый элемеет
-    else
-        return typeOf(0);
+        return vectorContains[0];           // Вернуть первый элемеет
+    else                                    // Если нет занятой памяти
+        throw std::out_of_range("MyVector::front(), Vector is empty");                 // Бросаем эксепшн
 }
 
 template<class typeOf>
-typeOf MyVector<typeOf>::back()
+typeOf& MyVector<typeOf>::back()
 {
     if(memory.used)                                         // Если есть занятая память
-        return vectorContains[memory.lastIndex - 1];     // Вернуть элемент на последнем месте
-    else
-        return typeOf(0);
+        return vectorContains[memory.lastIndex];            // Вернуть элемент на последнем месте
+    else                                                    // Если нет занятой памяти
+        throw std::out_of_range("MyVector::front(), Vector is empty");                 // Бросаем эксепшн
+}
+
+template<class typeOf>
+typeOf* MyVector<typeOf>::data()
+{
+    return vectorContains;
 }
 
 template<class typeOf>
@@ -130,17 +212,25 @@ void MyVector<typeOf>::updateMemory(int action)
     if (action == 0)                                // 0 действие - обновить свободную память
     {
         memory.free = memory.total - memory.used;
-        if(memory.free < 0)                         //Если оказалось, что свободной памяти меньше нуля
+        if(memory.free < 0)                         // Если оказалось, что свободной памяти меньше нуля
         {
             memory.free = 0;                        // Свободная пасять равна нулю
-            updateMemory(2);                     // Вызывается обновление памяти, со 2 действием
+            updateMemory(2);                        // Вызывается обновление памяти, со 2 действием
             memory.lastIndex = memory.used - 1;     // Обновляется последний индекс
         }
     }
-    else if (action == 1)                           //добавить память, и сразу её занять
+    else if (action == 1)                           // 1 действие - увеличить занятую память на 1
     {
-        memory.total += 1;
-        memory.used += 1;
+        if(memory.free)                             // Если память свободна
+        {
+            memory.total += 1;
+            updateMemory(0);                        // Обновить свободную память
+        }
+        else                                        // Если нет свободной памяти
+        {
+            memory.total += 1;
+            memory.used += 1;
+        }
     }
     else if (action == 2)                           // 2 действие - обновить занятую память
     {
@@ -149,6 +239,13 @@ void MyVector<typeOf>::updateMemory(int action)
             memory.used = memory.total;             // Использованная память уменьшается до общей
         }
     }
+}
+
+template<class typeOf>
+MyVector<typeOf> MyVector<typeOf>::operator=(const MyVector<typeOf> &Vector)
+{
+    this->memory = Vector.memory;
+    this->vectorContains = Vector.vectorContains;
 }
 
 template<class typeOf>
@@ -169,7 +266,7 @@ void MyVector<typeOf>::updateVector()
 
     delete[] vectorContains;
 
-    vectorContains = new typeOf[memory.used]; // Основной вектор создаётся по новой, но увеличивается в размере
+    vectorContains = new typeOf[memory.total + 1]; // Основной вектор создаётся по новой, но увеличивается в размере
     copyData(vectorContains, tempVector); // возвращается значение временного вектора
 
     delete [] tempVector;
--- myvector.h
+++ myvector.h
@@ -5,28 +5,40 @@ template <class typeOf>
 class MyVector
 {
     public:
-        MyVector();
-        ~MyVector();
-//                        ФУНКЦИИ РАЗМЕРА И ПАМЯТИ
-        unsigned int size() const noexcept;            // Возвращает колличество элементов в векторе
-        unsigned int capacity() const noexcept;        // Возвращает размер вектора
-        bool empty() const noexcept;
-        void resize(int size); // Изменяет размер массива на size
-        void shrink_to_fit();  // Сжимает размер, до совпадения
+        //                        КОНСТРУКТОРЫ И ДЕСТРУКТОРЫ
+        MyVector();                                             // Конструктор по умолчанию
+        MyVector(const MyVector &vector);                       // Конструтор заполнения
+        MyVector(typeOf fillElement, int repeat);               // Конструктор заполнения
+        MyVector(typeOf pushElement);                           // Конструктор заполнения с одним элементов
+        ~MyVector();                                            // Деструктор по умолчанию
+        //                        ФУНКЦИИ РАЗМЕРА И ПАМЯТИ
+        unsigned int size() const noexcept;                     // Возвращает колличество элементов в векторе
+        unsigned int capacity() const noexcept;                 // Возвращает размер вектора
+        bool empty() const noexcept;                            // Проверяет, пуст ли массив
+        void resize(int size);                                  // Изменяет размер массива на size
+        void upscale(int size);                                 // Увеличивает размер на size, с учётом свободной памяти
+        void shrink_to_fit() const noexcept;                    // Сжимает размер, до совпадения
 
-//                   ФУНКЦИИ, УПРАВЛЯЮЩИЕ ЭЛЕМЕНТАМИ В МАССИВЕ
-        void push_back(typeOf pushedBack); // Положить элемент в конец вектора
-        void pop_back();                   // Удаляет элемент в конце массива
-        void clear();                      // Очищает вектор
+        //                   ФУНКЦИИ, УПРАВЛЯЮЩИЕ ЭЛЕМЕНТАМИ В МАССИВЕ
+        void push_back(typeOf pushedBack);                      // Положить элемент в конец вектора
+        void push_back(typeOf pushedBack, int repeat);          // Положить элемент в конец списка несколько раз
+        void insert(typeOf element, int position);              // Вставить элемент на определённую позицию
+        void insert(typeOf element, int position, int repeat);  // Вставить repeat колличество элементов на опеределённую позцию
+        void pop_back();                                        // Удаляет элемент в конце массива
+        void clear();                                           // Очищает вектор
 
-//                      ДОСТАВАНИЕ ЭЛЕМЕНТА ИЗ МАССИВА-
-        typeOf at(int index);   // Вернуть элемент на i номере
-        typeOf front();         // Вернуть первый элемент массива
-        typeOf back();          // Вернуть последний элемент
+        //                      ДОСТАВАНИЕ ЭЛЕМЕНТА ИЗ МАССИВА-
+        typeOf &at(int index);                                  // Вернуть элемент на i номере
+        typeOf &front();                                        // Вернуть первый элемент массива
+        typeOf &back();                                         // Вернуть последний элемент
+        typeOf *data();                                         // Вернуть указатель на начало динамического массива
+
+        //                              ОПЕРАТОРЫ
+        MyVector<typeOf> operator= (const MyVector<typeOf> &vector);
 
         private:
-//                           ДАННЫЕ
-        struct sizeOfVector // Структура управления памятью
+        //                           ДАННЫЕ
+        struct sizeOfVector                                     // Структура управления памятью
         {
             int used;
             int lastIndex;
@@ -34,17 +46,17 @@ class MyVector
             int total;
         };
 
-        sizeOfVector memory = {0, -1, 0, 0}; // Создание структуры, и установка всего на нуль
+        sizeOfVector memory = {0, -1, 0, 0};                    // Создание структуры, и установка всего на нуль
 
-        typeOf * vectorContains = nullptr; // Создание массива, содержащего данные
+        typeOf * vectorContains = nullptr;                      // Создание массива, содержащего данные
 
-        protected:
-//                              ФУНКЦИИ
-        void updateMemory(int action); // Функция управления памятью
+        private:
+        //                              ФУНКЦИИ
+        void updateMemory(int action);                          // Функция управления памятью
 
-        void copyData(typeOf* mainVector, typeOf* tempVector); // Функция, копирующая один вектор в другой
+        void copyData(typeOf* mainVector, typeOf* tempVector);  // Функция, копирующая один вектор в другой
 
-        void updateVector();                                // Функция, обновляющая размер вектора
+        void updateVector();                                    // Функция, обновляющая размер вектора
 };
 
-#endif // MYVECTOR_H
+#endif  // MYVECTOR_H
